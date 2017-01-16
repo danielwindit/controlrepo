f2741d010b217609de3bd7913dbf504467e481c6
Daniel Wind <daniel.wind@prorail.nl>
2017-01-16 15:24:30 +0100

    fsd

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,9 +1,10 @@
 class profile::apache {
-  class {'::apache':
+  include apache 
+  include firewall
 
   firewall { '100 allow http and https access':
   dport  => [80, 443],
   proto  => tcp,
   action => accept,
- }
+  }
 }
