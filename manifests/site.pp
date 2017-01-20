5981acf843ad76d0ced213f7e59c2b6bd840847f
Daniel Wind <daniel.wind@prorail.nl>
2017-01-20 14:24:03 +0100

    fd

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,10 @@ node 'luamx6.ad.lab.post21.nl' {
   include profile::base
   include profile::apache
 } 
+
+node 'luamk1.ad.lab.post21.nl' {
+class { 'ansible':
+  ensure => 'master'
+  provider => 'automatic'
+ }
+}
